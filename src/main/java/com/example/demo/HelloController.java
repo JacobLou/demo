4fcb7f8b1e84be31be3af0cd7e8fba8ef9c61e71
louh <hao.lou@saintandrews.net>
2023-09-06 17:07:10 -0400

    finally worked

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,10 +4,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class HelloController {
+    public List<Vocab> vocabList = new ArrayList<>();
+    private int correctCount = 0;
+    private int totalCount = 0;
+    private Label accuracyLabel;
     @FXML
-    private Label questionsBar;
+    private Label questionsLabel;
     @FXML
     private Button buttonA;
     @FXML
@@ -16,41 +24,105 @@ public class HelloController {
     private Button buttonC;
     @FXML
     private Button buttonD;
+    @FXML
+    private Button submitButton;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        questionsBar.setText("Welcome to JavaFX Application!");
-    }
     public void initialize(){
-        Vocab.generateVocab();
+        vocabList = Vocab.generateVocab();
+        Collections.shuffle(vocabList);
         resetcolor();
-        String [] wordChoices =Gamelogic.generateWordChoices(Vocab.vocabList, Gamelogic.getVocab().word);
-        buttonA.setText(wordChoices[0]);
-        buttonB.setText(wordChoices[1]);
-        buttonC.setText(wordChoices[2]);
-        buttonD.setText(wordChoices[3]);
+        loadDefinitionAndChoices();
+        submitButton.setDisable(true);
     }
 
     public void buttonclicked(ActionEvent itemClicked){
         Button buttonclicked = (Button)itemClicked.getSource() ;
         resetcolor();
-        buttonclicked.setStyle("-fx-background-color: #FFFF00;-fx-text-fill: black;-fx-font-size: 14px;");
+        buttonclicked.setStyle("-fx-background-color: #d3d3f3;-fx-text-fill: black;-fx-font-size: 14px;");
     }
 
     public void submitClicked(ActionEvent itemClicked){
-        String [] wordChoices =Gamelogic.generateWordChoices(Vocab.vocabList, Gamelogic.getVocab().word);
-        buttonA.setText(wordChoices[0]);
-        buttonB.setText(wordChoices[1]);
-        buttonC.setText(wordChoices[2]);
-        buttonD.setText(wordChoices[3]);
 
     }
     public void resetcolor(){
-        buttonA.setStyle("-fx-background-color: #d3d3d3;-fx-text-fill: white;-fx-font-size: 14px;");
-        buttonB.setStyle("-fx-background-color: #d3d3d3;-fx-text-fill: white;-fx-font-size: 14px;");
-        buttonC.setStyle("-fx-background-color: #d3d3d3;-fx-text-fill: white;-fx-font-size: 14px;");
-        buttonD.setStyle("-fx-background-color: #d3d3d3;-fx-text-fill: white;-fx-font-size: 14px;");
+        buttonA.setStyle("-fx-background-color: #FFFFFF;-fx-text-fill: black;-fx-font-size: 14px;");
+        buttonB.setStyle("-fx-background-color: #FFFFFF;-fx-text-fill: black;-fx-font-size: 14px;");
+        buttonC.setStyle("-fx-background-color: #FFFFFF;-fx-text-fill: black;-fx-font-size: 14px;");
+        buttonD.setStyle("-fx-background-color: #FFFFFF;-fx-text-fill: black;-fx-font-size: 14px;");
     }
+
+    private void loadDefinitionAndChoices() {
+        if (!vocabList.isEmpty()) {
+            Vocab vocab = vocabList.remove(0);
+            String definition = vocab.definition;
+            String correctWord = vocab.word;
+
+
+            questionsLabel.setText(definition);
+
+            List<String> termChoices = generateRandomTermChoices(vocabList, correctWord);
+            Collections.shuffle(termChoices);
+
+            buttonA.setText(termChoices.get(0));
+            buttonB.setText(termChoices.get(1));
+            buttonC.setText(termChoices.get(2));
+            buttonD.setText(termChoices.get(3));
+/*
+            termBox.getChildren().clear();
+
+            for (String choice : termChoices) {
+                RadioButton termButton = new RadioButton(choice);
+                termButton.setToggleGroup(termGroup);
+                termButton.setStyle("-fx-font-size: 14px;");
+                termBox.getChildren().add(termButton);
+            }
+
+            // Enable the submit button
+            Button submitButton = (Button) ((VBox) accuracyLabel.getParent()).getChildren().get(2);
+            submitButton.setDisable(false);
+
+            // Clear the selection
+            termGroup.selectToggle(null);
+*/
+        } else {
+            // Game over, no more definitions left
+            questionsLabel.setText("Game Over! Thank you for playing.");
+        }
+    }
+
+    private List<String> generateRandomTermChoices(List<Vocab> vocabList, String correctWord) {
+        List<String> termChoices = new ArrayList<>();
+        Random random = new Random();
+
+        termChoices.add(correctWord);
+
+        for (int i = 0; i < 3; i++) {
+            int index = random.nextInt(vocabList.size());
+            String incorrectWord = vocabList.get(index).word;
+            termChoices.add(incorrectWord);
+        }
+
+        return termChoices;
+    }
+
+//    private void checkAnswer() {
+//        Button selectedTerm = (Button) termGroup.getSelectedToggle();
+//
+//        if (selectedTerm != null) {
+//            String selectedTermText = selectedTerm.getText();
+//
+//            if (selectedTermText.equals(vocabList.get(0).word)) {
+//                correctCount++;
+//                accuracyLabel.setText("Accuracy: " + getAccuracyString(correctCount, totalCount));
+//                accuracyLabel.setStyle("-fx-text-fill: " + getAccuracyColor(correctCount, totalCount) + "; -fx-font-weight: bold;");
+//            }
+//
+//            totalCount++;
+//            loadDefinitionAndChoices();
+//        }
+//    }
 
 
 }
