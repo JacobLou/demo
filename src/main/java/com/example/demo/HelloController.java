b227d34a7f25fce599310cbe66da55e6d9219fa6
louh <hao.lou@saintandrews.net>
2023-09-06 21:03:00 -0400

    crashed again

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Label;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class HelloController {
     public List<Vocab> vocabList = new ArrayList<>();
@@ -32,17 +31,22 @@ public class HelloController {
 
     private Button selectedButton;
 
-
     @FXML
     public void initialize(){
         vocabList = Vocab.generateVocab();
-        wordChoice = vocabList;
+        wordChoice = new ArrayList<>(vocabList); // Make a copy of vocabList
         submitButton.setDisable(true);
         Collections.shuffle(vocabList);
         resetcolor();
-        loadDefinitionAndChoices();
-        //submitButton.setDisable(true);
+
+        if (!vocabList.isEmpty()) {
+            loadDefinitionAndChoices();
+        } else {
+            // If there are no words, end the game immediately
+            endGame();
+        }
     }
+
 
     public void buttonclicked(ActionEvent itemClicked){
         Button buttonclicked = (Button)itemClicked.getSource() ;
@@ -80,22 +84,14 @@ public class HelloController {
 
             submitButton.setDisable(false);
 
-        } else {
-            questionsLabel.setText("Game Over! Thank you for playing.");
-            questionsLabel.setStyle("-fx-text-fill: green; -fx-font-weight: bold;");
-
-            buttonA.setDisable(true);
-            buttonB.setDisable(true);
-            buttonC.setDisable(true);
-            buttonD.setDisable(true);
-
+        }else {
+            endGame();
         }
     }
 
 
     private List<String> generateRandomTermChoices(List<Vocab> vocabList, String correctWord) {
         List<String> termChoices = new ArrayList<>();
-        Random random = new Random();
 
         termChoices.add(correctWord);
 
@@ -131,6 +127,8 @@ public class HelloController {
             accuracyLabel.setStyle("-fx-text-fill: " + getAccuracyColor(correctCount, totalCount) + "; -fx-font-weight: bold;");
 
             loadDefinitionAndChoices();
+
+
         }
     }
 
@@ -152,6 +150,17 @@ public class HelloController {
         } else {
             return "green";
         }
+    }
+
+    private void endGame() {
+        questionsLabel.setText("Game Over! Thank you for playing.");
+        questionsLabel.setStyle("-fx-text-fill: green; -fx-font-weight: bold;");
+
+        buttonA.setDisable(true);
+        buttonB.setDisable(true);
+        buttonC.setDisable(true);
+        buttonD.setDisable(true);
+        submitButton.setDisable(true);
     }
 
 }
